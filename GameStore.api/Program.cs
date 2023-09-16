9874c84697b7e85dfd45b0b4619473a3b1f171d8
Perera Dev <nashi.codes@gmail.com>
2023-09-16 01:49:08 -0300

    GameRepository Interface  not implemented as Services, explication

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,8 +1,15 @@
 using GameStore.api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-
+/*
+ ** As I said in the IGameRespository.cs file
+ ** I could Abstract the GameRespository class in the IGameRespository interface
+ ** And register the interface in the services to inject the class in the endpoints
+ ** But since there is only Group of endpoints that use the GameRespository class
+ ** And the usability of this service is so specific
+ ** I decided to use GameRespository class directly in the endpoints using static methods
+ ** This way I don't need to create a new instance of GameRespository class in each endpoint
+ */
 var app = builder.Build();
 
 app.MapGamesEndpoints();
---
+++ GameStore.api/Repositories/IGameRespository.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStore.api.Repositories;
+
+public interface IGameRespository
+{
+    /*
+     ** I could Abstract the GameRespository class and use this interface to inject the class in the endpoints
+     ** But since there is only Group of endpoints that use the GameRespository class
+     ** I decided to use GameRespository class directly in the endpoints using static methods
+     ** This way I don't need to create a new instance of GameRespository class in each endpoint
+     */
+}
